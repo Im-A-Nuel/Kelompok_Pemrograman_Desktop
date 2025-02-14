785c74d6943cbdc27c183687b3aa01d244c70e75
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2025-02-14 12:56:45 +0700

    update asset

---
+++ #Tugas 1/Game Whack a Mole/Game Whack a Mole/Form2.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        BackgroundWorker1 = New ComponentModel.BackgroundWorker()
+        pbRefresh = New PictureBox()
+        pbMole1 = New PictureBox()
+        pbMole2 = New PictureBox()
+        pbMole3 = New PictureBox()
+        pbMole4 = New PictureBox()
+        pbMole5 = New PictureBox()
+        pbMole6 = New PictureBox()
+        pbMole7 = New PictureBox()
+        CType(pbRefresh, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pbMole1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pbMole2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pbMole3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pbMole4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pbMole5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pbMole6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(pbMole7, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' pbRefresh
+        ' 
+        pbRefresh.BackColor = Color.Transparent
+        pbRefresh.BackgroundImage = CType(resources.GetObject("pbRefresh.BackgroundImage"), Image)
+        pbRefresh.BackgroundImageLayout = ImageLayout.Stretch
+        pbRefresh.Location = New Point(19, 24)
+        pbRefresh.Name = "pbRefresh"
+        pbRefresh.Size = New Size(65, 62)
+        pbRefresh.TabIndex = 0
+        pbRefresh.TabStop = False
+        ' 
+        ' pbMole1
+        ' 
+        pbMole1.BackColor = Color.Transparent
+        pbMole1.BackgroundImage = CType(resources.GetObject("pbMole1.BackgroundImage"), Image)
+        pbMole1.BackgroundImageLayout = ImageLayout.Stretch
+        pbMole1.Location = New Point(314, 214)
+        pbMole1.Name = "pbMole1"
+        pbMole1.Size = New Size(134, 127)
+        pbMole1.TabIndex = 1
+        pbMole1.TabStop = False
+        pbMole1.Visible = False
+        ' 
+        ' pbMole2
+        ' 
+        pbMole2.BackColor = Color.Transparent
+        pbMole2.BackgroundImage = CType(resources.GetObject("pbMole2.BackgroundImage"), Image)
+        pbMole2.BackgroundImageLayout = ImageLayout.Stretch
+        pbMole2.Location = New Point(62, 288)
+        pbMole2.Name = "pbMole2"
+        pbMole2.Size = New Size(134, 127)
+        pbMole2.TabIndex = 2
+        pbMole2.TabStop = False
+        pbMole2.Visible = False
+        ' 
+        ' pbMole3
+        ' 
+        pbMole3.BackColor = Color.Transparent
+        pbMole3.BackgroundImage = CType(resources.GetObject("pbMole3.BackgroundImage"), Image)
+        pbMole3.BackgroundImageLayout = ImageLayout.Stretch
+        pbMole3.Location = New Point(465, 352)
+        pbMole3.Name = "pbMole3"
+        pbMole3.Size = New Size(134, 127)
+        pbMole3.TabIndex = 3
+        pbMole3.TabStop = False
+        pbMole3.Visible = False
+        ' 
+        ' pbMole4
+        ' 
+        pbMole4.BackColor = Color.Transparent
+        pbMole4.BackgroundImage = CType(resources.GetObject("pbMole4.BackgroundImage"), Image)
+        pbMole4.BackgroundImageLayout = ImageLayout.Stretch
+        pbMole4.Location = New Point(187, 410)
+        pbMole4.Name = "pbMole4"
+        pbMole4.Size = New Size(134, 127)
+        pbMole4.TabIndex = 4
+        pbMole4.TabStop = False
+        pbMole4.Visible = False
+        ' 
+        ' pbMole5
+        ' 
+        pbMole5.BackColor = Color.Transparent
+        pbMole5.BackgroundImage = CType(resources.GetObject("pbMole5.BackgroundImage"), Image)
+        pbMole5.BackgroundImageLayout = ImageLayout.Stretch
+        pbMole5.Location = New Point(492, 502)
+        pbMole5.Name = "pbMole5"
+        pbMole5.Size = New Size(134, 127)
+        pbMole5.TabIndex = 5
+        pbMole5.TabStop = False
+        pbMole5.Visible = False
+        ' 
+        ' pbMole6
+        ' 
+        pbMole6.BackColor = Color.Transparent
+        pbMole6.BackgroundImage = CType(resources.GetObject("pbMole6.BackgroundImage"), Image)
+        pbMole6.BackgroundImageLayout = ImageLayout.Stretch
+        pbMole6.Location = New Point(52, 577)
+        pbMole6.Name = "pbMole6"
+        pbMole6.Size = New Size(134, 127)
+        pbMole6.TabIndex = 6
+        pbMole6.TabStop = False
+        pbMole6.Visible = False
+        ' 
+        ' pbMole7
+        ' 
+        pbMole7.BackColor = Color.Transparent
+        pbMole7.BackgroundImage = CType(resources.GetObject("pbMole7.BackgroundImage"), Image)
+        pbMole7.BackgroundImageLayout = ImageLayout.Stretch
+        pbMole7.Location = New Point(329, 661)
+        pbMole7.Name = "pbMole7"
+        pbMole7.Size = New Size(134, 127)
+        pbMole7.TabIndex = 7
+        pbMole7.TabStop = False
+        pbMole7.Visible = False
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(684, 845)
+        Controls.Add(pbMole7)
+        Controls.Add(pbMole6)
+        Controls.Add(pbMole5)
+        Controls.Add(pbMole4)
+        Controls.Add(pbMole3)
+        Controls.Add(pbMole2)
+        Controls.Add(pbMole1)
+        Controls.Add(pbRefresh)
+        DoubleBuffered = True
+        Name = "Form2"
+        Text = "Whack a Mole!"
+        CType(pbRefresh, ComponentModel.ISupportInitialize).EndInit()
+        CType(pbMole1, ComponentModel.ISupportInitialize).EndInit()
+        CType(pbMole2, ComponentModel.ISupportInitialize).EndInit()
+        CType(pbMole3, ComponentModel.ISupportInitialize).EndInit()
+        CType(pbMole4, ComponentModel.ISupportInitialize).EndInit()
+        CType(pbMole5, ComponentModel.ISupportInitialize).EndInit()
+        CType(pbMole6, ComponentModel.ISupportInitialize).EndInit()
+        CType(pbMole7, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents pbRefresh As PictureBox
+    Friend WithEvents pbMole1 As PictureBox
+    Friend WithEvents pbMole2 As PictureBox
+    Friend WithEvents pbMole3 As PictureBox
+    Friend WithEvents pbMole4 As PictureBox
+    Friend WithEvents pbMole5 As PictureBox
+    Friend WithEvents pbMole6 As PictureBox
+    Friend WithEvents pbMole7 As PictureBox
+End Class
--- #Tugas 1/Game Whack a Mole/Game Whack a Mole/obj/Debug/net8.0-windows/Game Whack a Mole.AssemblyInfo.vb
+++ #Tugas 1/Game Whack a Mole/Game Whack a Mole/obj/Debug/net8.0-windows/Game Whack a Mole.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("Game Whack a Mole"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07626223b99ba3df05084f4f9f1c879308f9e6c0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207942416cb24ba5fe9642ea953924160f15aca8"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("Game Whack a Mole"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("Game Whack a Mole"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
